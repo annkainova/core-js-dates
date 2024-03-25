a0f72f740bf211955c384bd36a8f512360a57eb7
Anna <cainowa@gmail.com>
2024-03-25 13:53:48 +0100

    feat: solve 2 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,8 +32,10 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const event = new Date(date);
+  const time = event.toTimeString().slice(0, 8);
+  return time;
 }
 
 /**
@@ -47,10 +49,15 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const event = new Date(date);
+  const options = {
+    weekday: 'long',
+  };
+  const value = event.toLocaleDateString('en', options);
+  return value;
 }
-
+getDayName('01 Jan 1970 00:00:00 UTC');
 /**
  * Returns the date of the next Friday from a given date.
  *
